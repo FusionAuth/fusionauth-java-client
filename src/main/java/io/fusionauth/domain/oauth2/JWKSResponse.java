3541355dd0f66a4e2fdcb45747c12c2b7896d5e5
Daniel DeGroff <daniel@fusionauth.io>
2019-10-21 09:24:46 -0600

    Return an empty collection for the JWKS endpoint, some integrations don't expect the 'keys' property to be missing.

--- src/main/java/io/fusionauth/domain/oauth2/JWKSResponse.java
+++ src/main/java/io/fusionauth/domain/oauth2/JWKSResponse.java
@@ -5,6 +5,7 @@ package io.fusionauth.domain.oauth2;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.inversoft.json.JacksonConstructor;
 import io.fusionauth.domain.Buildable;
 import io.fusionauth.jwks.domain.JSONWebKey;
@@ -13,6 +14,8 @@ import io.fusionauth.jwks.domain.JSONWebKey;
  * @author Daniel DeGroff
  */
 public class JWKSResponse implements Buildable<JWKSResponse> {
+  // Return an empty collection for this endpoint
+  @JsonInclude(JsonInclude.Include.NON_ABSENT)
   public List<JSONWebKey> keys;
 
   @JacksonConstructor
