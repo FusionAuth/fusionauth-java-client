f3d02fd67ccf3cd52f25aeac9a2349e49b17c17e
Daniel DeGroff <daniel@fusionauth.io>
2020-09-25 15:32:14 -0600

    Rename method per feedback. https://github.com/FusionAuth/fusionauth-python-client/issues/8#issuecomment-699137479

--- src/main/java/io/fusionauth/client/FusionAuthClient.java
+++ src/main/java/io/fusionauth/client/FusionAuthClient.java
@@ -1321,6 +1321,24 @@ public class FusionAuthClient {
   }
 
   /**
+   * Inspect an access token issued by FusionAuth.
+   *
+   * @param client_id The unique client identifier. The client Id is the Id of the FusionAuth Application for which this token was generated.
+   * @param token The access token returned by this OAuth provider as the result of a successful authentication.
+   * @return The ClientResponse object.
+   */
+  public ClientResponse<IntrospectResponse, OAuthError> introspectAccessToken(String client_id, String token) {
+    Map<String, String> parameters = new HashMap<>();
+    parameters.put("client_id", client_id);
+    parameters.put("token", token);
+    return startAnonymous(IntrospectResponse.class, OAuthError.class)
+        .uri("/oauth2/introspect")
+        .bodyHandler(new FormDataBodyHandler(parameters))
+        .post()
+        .go();
+  }
+
+  /**
    * Issue a new access token (JWT) for the requested Application after ensuring the provided JWT is valid. A valid
    * access token is properly signed and not expired.
    * <p>
@@ -1431,24 +1449,6 @@ public class FusionAuthClient {
         .urlSegment(actionId)
         .bodyHandler(new JSONBodyHandler(request, objectMapper))
         .put()
-        .go();
-  }
-
-  /**
-   * Inspect an access token issued by FusionAuth.
-   *
-   * @param client_id The unique client identifier. The client Id is the Id of the FusionAuth Application for which this token was generated.
-   * @param token The access token returned by this OAuth provider as the result of a successful authentication.
-   * @return The ClientResponse object.
-   */
-  public ClientResponse<IntrospectResponse, OAuthError> oauth2Introspect(String client_id, String token) {
-    Map<String, String> parameters = new HashMap<>();
-    parameters.put("client_id", client_id);
-    parameters.put("token", token);
-    return startAnonymous(IntrospectResponse.class, OAuthError.class)
-        .uri("/oauth2/introspect")
-        .bodyHandler(new FormDataBodyHandler(parameters))
-        .post()
         .go();
   }
 
