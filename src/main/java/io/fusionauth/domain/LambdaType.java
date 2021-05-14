81c9121b3a2d2a028313561b5734bd8e560a686f
Brett Pontarelli <brett@paperyfrog.com>
2021-05-13 23:35:37 -0700

    Use "and" instead

--- src/main/java/io/fusionauth/domain/LambdaType.java
+++ src/main/java/io/fusionauth/domain/LambdaType.java
@@ -18,8 +18,7 @@ package io.fusionauth.domain;
 /**
  * The types of lambdas that indicate how they are invoked by FusionAuth.
  *
- * @author Brian Pontarelli
- * @author Brett Pontarelli
+ * @author Brian Pontarelli and Brett Pontarelli
  */
 public enum LambdaType {
   // [brettp]TODO: HYPRReconcile and LDAPConnectorReconcile have slightly different text, but
