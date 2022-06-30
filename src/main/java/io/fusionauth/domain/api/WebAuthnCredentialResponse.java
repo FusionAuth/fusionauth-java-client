5ae6f9d3611c24381df3ad643ec45c47376792c2
Spencer Witt <spencer@fusionauth.io>
2022-06-30 10:10:11 -0500

    add WebAuthnCredentialResponse to the client domain

---
+++ src/main/java/io/fusionauth/domain/api/WebAuthnCredentialResponse.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022, FusionAuth, All Rights Reserved
+ */
+package io.fusionauth.domain.api;
+
+import com.inversoft.json.JacksonConstructor;
+import io.fusionauth.domain.WebAuthnCredential;
+
+import java.util.List;
+
+/**
+ * WebAuthn Credential API response
+ *
+ * @author Spencer Witt
+ */
+public class WebAuthnCredentialResponse {
+  public WebAuthnCredential webauthnCredential;
+
+  public List<WebAuthnCredential> webauthnCredentials;
+
+  @JacksonConstructor
+  public WebAuthnCredentialResponse() {
+  }
+
+  public WebAuthnCredentialResponse(WebAuthnCredential webauthnCredential) {
+    this.webauthnCredential = webauthnCredential;
+  }
+
+  public WebAuthnCredentialResponse(List<WebAuthnCredential> webauthnCredentials) {
+    this.webauthnCredentials = webauthnCredentials;
+  }
+}
