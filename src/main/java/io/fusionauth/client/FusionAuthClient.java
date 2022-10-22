423e6e83298c461e4c60ba93948b696019c47090
Daniel DeGroff <daniel@fusionauth.io>
2022-10-21 23:03:31 -0600

    domain sync

--- src/main/java/io/fusionauth/client/FusionAuthClient.java
+++ src/main/java/io/fusionauth/client/FusionAuthClient.java
@@ -431,6 +431,27 @@ public class FusionAuthClient {
   }
 
   /**
+   * Make a Client Credentials grant request to obtain an access token.
+   *
+   * @param client_id The client identifier. The client Id is the Id of the FusionAuth Entity in which you you are attempting to authenticate.
+   * @param client_secret The client secret used to authenticate this request.
+   * @param scope (Optional) This parameter is used to indicate which target entity you are requesting access. To request access to an entity, use the format target-entity:<target-entity-id>:<roles>. Roles are an optional comma separated list.
+   * @return The ClientResponse object.
+   */
+  public ClientResponse<AccessToken, OAuthError> clientCredentialsGrant(String client_id, String client_secret, String scope) {
+    Map<String, List<String>> parameters = new HashMap<>();
+    parameters.put("client_id", Arrays.asList(client_id));
+    parameters.put("client_secret", Arrays.asList(client_secret));
+    parameters.put("grant_type", Arrays.asList("client_credentials"));
+    parameters.put("scope", Arrays.asList(scope));
+    return startAnonymous(AccessToken.class, OAuthError.class)
+        .uri("/oauth2/token")
+        .bodyHandler(new FormDataBodyHandler(parameters))
+        .post()
+        .go();
+  }
+
+  /**
    * Adds a comment to the user's account.
    *
    * @param request The request object that contains all the information used to create the user comment.
--- src/main/java/io/fusionauth/domain/api/webauthn/PublicKeyCredentialType.java
+++ src/main/java/io/fusionauth/domain/api/webauthn/PublicKeyCredentialType.java
@@ -3,7 +3,11 @@
  */
 package io.fusionauth.domain.api.webauthn;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 /**
  * Defines valid credential types. This is an extension point in the WebAuthn spec. The only defined value at this time is "public-key"
@@ -14,6 +18,25 @@ public enum PublicKeyCredentialType {
   /**
    * The associated credential represents an asymmetric key pair
    */
-  @JsonProperty("public-key")
-  publicKey
+  publicKey("public-key");
+
+  private static final Map<String, PublicKeyCredentialType> valueMap = new HashMap<>();
+
+  @JsonValue
+  public final String value;
+
+  PublicKeyCredentialType(String value) {
+    this.value = value;
+  }
+
+  @JsonCreator
+  public static PublicKeyCredentialType forValue(int value) {
+    return valueMap.get(value);
+  }
+
+  static {
+    for (PublicKeyCredentialType k : values()) {
+      valueMap.put(k.value, k);
+    }
+  }
 }
