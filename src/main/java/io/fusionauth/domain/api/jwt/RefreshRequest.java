e83fab773ff880e24da74f73c91487f4792b3551
Daniel DeGroff <daniel@fusionauth.io>
2023-03-28 17:49:38 -0600

    domain sync

--- src/main/java/io/fusionauth/domain/api/jwt/RefreshRequest.java
+++ src/main/java/io/fusionauth/domain/api/jwt/RefreshRequest.java
@@ -1,28 +1,17 @@
 /*
- * Copyright (c) 2018, FusionAuth, All Rights Reserved
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific
- * language governing permissions and limitations under the License.
+ * Copyright (c) 2018-2023, FusionAuth, All Rights Reserved
  */
 package io.fusionauth.domain.api.jwt;
 
 import com.inversoft.json.JacksonConstructor;
+import io.fusionauth.domain.Buildable;
 import io.fusionauth.domain.EventInfo;
 import io.fusionauth.domain.api.BaseEventRequest;
 
 /**
  * @author Daniel DeGroff
  */
-public class RefreshRequest extends BaseEventRequest {
+public class RefreshRequest extends BaseEventRequest implements Buildable<RefreshRequest> {
   public String refreshToken;
 
   public String token;
